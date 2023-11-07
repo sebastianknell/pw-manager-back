537fffa7dd6f267d841bb6b259980fe153abe943
Sebastian Knell <sebastianknell@hotmail.com>
2023-11-06 19:55:05 -0500

    auth and get passwords example

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken")
+const fs = require("fs");
 const app = express();
 
 app.use(cors());
@@ -14,6 +16,7 @@ app.use((req, res, next) => {
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 const srp = require('secure-remote-password/server')
+const tokenSecret = "youraccesstokensecret";
 
 app.post("/register", async (req, res) => {
     const username = req.body.username;
@@ -34,7 +37,7 @@ app.post("/generate", async (req, res) => {
     const username = req.body.username;
     const ephemeral = req.body.ephemeral;
 
-    const user = await prisma.user.findUniqueOrThrow({
+    const user = await prisma.user.findUnique({
         where: {
             username: username,
         },
@@ -44,6 +47,7 @@ app.post("/generate", async (req, res) => {
         },
     });
 
+    // TODO: caso cuando el usuario no existe
     const serverEphemeral = srp.generateEphemeral(user.verifier);
 
     await prisma.user.update({
@@ -72,14 +76,55 @@ app.post("/login", async (req, res) => {
         },
     });
 
-    const serverSession = srp.deriveSession(user.serverSecretEphemeral, user.clientPublicEphemeral, user.salt, username, user.verifier, proof);
-    console.log(serverSession.key);
+    try {
+        const serverSession = srp.deriveSession(user.serverSecretEphemeral, user.clientPublicEphemeral, user.salt, username, user.verifier, proof);
+        console.log(serverSession.key);
 
-    res.json({
-        proof: serverSession.proof,
-    })
+        const token = jwt.sign({userId: user.userId, username}, tokenSecret);
+        console.log(token);
+        res.json({
+            proof: serverSession.proof,
+            token,
+        });
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(403);
+    }
+});
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, tokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+app.get("/getpasswords", authenticateJWT, async (req, res) => {
+    const { userId} = req.user;
+
+    const userData = await prisma.user.findFirst({
+        where: {
+            userId: userId
+        },
+        select: {
+            dataPath: true,
+        }
+    });
+
+    const data = fs.readFileSync(userData.dataPath);
+    res.json(JSON.parse(data));
 })
 
 app.listen(5050, () => {
     console.log('Server listening')
-})
+});
--- test.js
+++ test.js
@@ -1,6 +1,18 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const fs = require("fs")
 
-prisma.user.findMany().then(data => {
-    console.log(data)
-})
+// prisma.user.update({
+//     where: {
+//         username: "sebastianknell"
+//     },
+//     data: {
+//         dataPath: "./data/aa.json"
+//     }
+// }).then(data => {
+//     console.log(data)
+// })
+
+const data = fs.readFileSync("./data/aa.json")
+console.log(data.toString())
+console.log(JSON.parse(data)[0].web)
