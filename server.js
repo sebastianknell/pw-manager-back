a5282d34796ca1c31276e90cae686d8614c193db
Sebastian Knell <sebastianknell@hotmail.com>
2023-11-10 11:40:44 -0500

    save passwords as json

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const app = express();
 const crypto = require("crypto-js");
-const copyPaste = require('copy-paste');
+const copyPaste = require("copy-paste");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  console.log(`${req.method} request to ${req.url}`);
-  next();
+    console.log(`${req.method} request to ${req.url}`);
+    next();
 });
 
-const { PrismaClient } = require("@prisma/client")
-const prisma = new PrismaClient()
-const srp = require('secure-remote-password/server')
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const srp = require("secure-remote-password/server");
+const tokenSecret = "youraccesstokensecret";
 
 app.post("/register", async (req, res) => {
     const username = req.body.username;
@@ -43,7 +44,10 @@ app.post("/register", async (req, res) => {
             console.log("Clave copiada al portapapeles");
         });
     } catch (error) {
-        console.error("Error al copiar la clave al portapapeles:", error.message);
+        console.error(
+            "Error al copiar la clave al portapapeles:",
+            error.message
+        );
     }
 
     res.send();
@@ -51,44 +55,43 @@ app.post("/register", async (req, res) => {
 app.post("/generate", async (req, res) => {
     const username = req.body.username;
     const ephemeral = req.body.ephemeral;
-  
+
     const user = await prisma.user.findUnique({
-      where: {
-        username: username,
-      },
-      select: {
-        salt: true,
-        verifier: true,
-        secretKey: true, // Asegúrate de incluir la secretKey en la respuesta
-      },
-    });
-  
-    if (user) {
-      const serverEphemeral = srp.generateEphemeral(user.verifier);
-  
-      await prisma.user.update({
         where: {
-          username: username,
+            username: username,
         },
-        data: {
-          serverSecretEphemeral: serverEphemeral.secret,
-          clientPublicEphemeral: ephemeral,
+        select: {
+            salt: true,
+            verifier: true,
+            secretKey: true, // Asegúrate de incluir la secretKey en la respuesta
         },
-      });
-  
-      res.json({
-        salt: user.salt,
-        ephemeral: serverEphemeral.public,
-        user: {
-        secretKey: user.secretKey, // Incluye la secretKey en la respuesta
-        },
-      });
+    });
+
+    if (user) {
+        const serverEphemeral = srp.generateEphemeral(user.verifier);
+
+        await prisma.user.update({
+            where: {
+                username: username,
+            },
+            data: {
+                serverSecretEphemeral: serverEphemeral.secret,
+                clientPublicEphemeral: ephemeral,
+            },
+        });
+
+        res.json({
+            salt: user.salt,
+            ephemeral: serverEphemeral.public,
+            user: {
+                secretKey: user.secretKey, // Incluye la secretKey en la respuesta
+            },
+        });
     } else {
-      // Manejo de error si el usuario no existe
-      res.status(404).json({ error: "Usuario no encontrado" });
+        // Manejo de error si el usuario no existe
+        res.status(404).json({ error: "Usuario no encontrado" });
     }
-  });
-  
+});
 
 app.post("/login", async (req, res) => {
     const username = req.body.username;
@@ -102,7 +105,14 @@ app.post("/login", async (req, res) => {
 
     try {
         // Use la secretKey almacenada en la base de datos como contraseña durante la autenticación
-        const serverSession = srp.deriveSession(user.secretKey, user.clientPublicEphemeral, user.salt, username, user.verifier, proof);
+        const serverSession = srp.deriveSession(
+            user.secretKey,
+            user.clientPublicEphemeral,
+            user.salt,
+            username,
+            user.verifier,
+            proof
+        );
         console.log(serverSession.key);
 
         const token = jwt.sign({ userId: user.userId, username }, tokenSecret);
@@ -116,7 +126,6 @@ app.post("/login", async (req, res) => {
         res.sendStatus(403);
     }
 });
-
 
 const authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
@@ -148,14 +157,30 @@ app.get("/getpasswords", authenticateJWT, async (req, res) => {
     });
 
     if (!userData || !userData.dataPath) {
-        return res.status(404).json({ error: "Ruta de datos no válida." });
+        return res.json({ passwordData: "" });
     }
 
-    const data = fs.readFileSync(userData.dataPath);
-    res.json(JSON.parse(data));
+    // const data = fs.readFileSync(userData.dataPath);
+    return res.json({ passwordData: userData.dataPath });
 });
 
+app.post("/savepasswords", authenticateJWT, async (req, res) => {
+    const { userId } = req.user;
+    const passwordData = req.body.passwordData;
+    console.log(passwordData);
+
+    await prisma.user.update({
+        where: {
+            userId: userId,
+        },
+        data: {
+            dataPath: passwordData,
+        },
+    });
+
+    res.send();
+});
 
 app.listen(5050, () => {
-    console.log('Server listening')
+    console.log("Server listening");
 });
--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-prisma.user.findMany().then(data => {
-    console.log(data)
-})
+prisma.user
+    .update({
+        where: {
+            username: "guille2400",
+        },
+        data: {
+            dataPath: null,
+        },
+    })
+    .then((data) => console.log(data));
+
+// prisma.user
+//     .findMany()
+//     .then((data) => console.log(data));
